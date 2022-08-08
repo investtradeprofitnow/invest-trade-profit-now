9dd838db98599b42aeb9f7b96551aa58a1c10f2a
Invest TRade Profit Now <sddmsinvesttradeprofitnow@gmail.com>
2022-08-08 18:40:08 +0530

    Strategy Short updated

--- resources/views/admin/strategy-short/add-strategy.blade.php
+++ resources/views/admin/strategy-short/add-strategy.blade.php
@@ -1,23 +1,23 @@
 @extends('admin.layouts.app')
-@section('pageTitle', 'Add Strategy List')
+@section('pageTitle', 'Add Strategy')
 @section('content')
     <div class="p-2 active-cont">
-	    <h1 class="mb-3 text-center section-title">Add Team Member</h1>
+	    <h1 class="mb-3 text-center section-title">Add Strategy</h1>
         <div class="form p-4 mx-auto" style="width: 70%">
-            <form id="add-member-form" method="post" action="/saveStrategyShort">
+            <form id="add-strategy-short-form" method="post" action="/admin/save-strategy-short" enctype="multipart/form-data">
             {{ csrf_field() }}
             <div class="form-group">
                 <label for="name">Name:</label>
                 <input type="text" class="form-control" name="name" id="name" maxlength="50" required>
             </div>
             <div class="form-group mt-3">
-                <label for="designation">Description:</label>
+                <label for="desc">Description:</label>
                 <textarea class="form-control" name="desc" id="desc" required></textarea>
             </div>
             <div class="form-group mt-3">
-                <label for="photo" class="form-label">Service Name:</label>
-                <select class="form-control" name="service" id="service">
-                    <option value="Intraday">Intraday</option>
+                <label for="type" class="form-label">Type:</label>
+                <select class="form-control" name="type" id="type">
+                    <option value="Intraday" selected>Intraday</option>
                     <option value="BTST">BTST</option>
                     <option value="Positional">Positional</option>
                     <option value="Investment">Investment</option>
@@ -25,7 +25,7 @@
             </div>
             <div class="form-group mt-3">
                 <label for="video" class="form-label">Video:</label>
-                <input type="file" class="form-control form-control-sm" name="video" id="video" required>
+                <input type="file" class="form-control form-control-sm" name="video" id="video" accept="video/*" required>
             </div>
             <div class="form-group mt-3">
                 <input type="submit" class="btn btn-outline" value="Save Strategy"/>
---
+++ resources/views/admin/strategy-short/edit-strategy.blade.php
@@ -0,0 +1,39 @@
+@extends('admin.layouts.app')
+@section('pageTitle', 'Edit Strategy')
+@section('content')
+    <div class="p-2 active-cont">
+	    <h1 class="mb-3 text-center section-title">Edit Strategy</h1>
+        <div class="form p-4 mx-auto" style="width: 70%">
+            <form id="add-strategy-short-form" method="post" action="/admin/update-strategy-short" enctype="multipart/form-data">
+            {{ csrf_field() }}
+            <div class="form-group">
+                <label for="name">Name:</label>
+                <input type="text" class="form-control" name="name" id="name" maxlength="50" value="{{$strategy->name}}" required>
+            </div>
+            <div class="form-group mt-3">
+                <label for="desc">Description:</label>
+                <textarea class="form-control" name="desc" id="desc" required>{{$strategy->description}}</textarea>
+            </div>
+            <div class="form-group mt-3">
+                <label for="type" class="form-label">Type:</label>
+                <select class="form-control" name="type" id="type">
+                    <option value="Intraday" {{$strategy->type=='Intraday'?'selected':''}}>Intraday</option>
+                    <option value="BTST" {{$strategy->type=='BTST'?'selected':''}}>BTST</option>
+                    <option value="Positional" {{$strategy->type=='Positional'?'selected':''}}>Positional</option>
+                    <option value="Investment" {{$strategy->type=='Investment'?'selected':''}}>Investment</option>
+                </select>
+            </div>
+            <div class="form-group mt-3">
+                <label for="video" class="form-label">Video:</label>
+                <br/>Current Video: {{$strategy->video}}
+                <input type="file" class="form-control form-control-sm" name="video" id="video" accept="video/*">
+            </div>
+            <div>
+                <input type="hidden" name="id" value="{{$strategy->id}}"/>
+            </div>
+            <div class="form-group mt-3">
+                <input type="submit" class="btn btn-outline" value="Update Strategy"/>
+            </div>
+        </div>
+    </div>
+@stop
