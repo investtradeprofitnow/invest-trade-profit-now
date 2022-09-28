22124a7edd1b73819c441391e259b8ba4aa036a4
Invest TRade Profit Now <sddmsinvesttradeprofitnow@gmail.com>
2022-09-28 15:10:33 +0530

    Max 5 digits for price

--- resources/views/admin/strategy-short/edit-strategy.blade.php
+++ resources/views/admin/strategy-short/edit-strategy.blade.php
@@ -30,7 +30,7 @@
             </div>
             <div class="form-group mt-3">
                 <label for="price">Price:</label>
-                <input type="text" class="form-control" name="price" id="price" value="{{$strategy->price}}" maxlength="6" oninput="this.value = this.value.replace(/[^0-9]/g, '').replace(/(\..*)\./g, '$1');"required/>
+                <input type="text" class="form-control" name="price" id="price" value="{{$strategy->price}}" maxlength="5" oninput="this.value = this.value.replace(/[^0-9]/g, '').replace(/(\..*)\./g, '$1');"required/>
             </div>
             <div class="form-group mt-3">
                 <label for="brief" class="form-label">Brief Strategy:</label>
