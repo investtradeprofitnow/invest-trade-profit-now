a61ad227a21fc00f694185378915f3c40adea8aa
Invest TRade Profit Now <sddmsinvesttradeprofitnow@gmail.com>
2022-10-03 12:46:01 +0530

    Dynamic plan pricing

---
+++ config/payments.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    "razorpayId"    => env("RAZORPAY_ID", "ABCDEF"),
+    "razorpayKey"   => env("RAZORPAY_KEY", "ABCDEF"),
+    "currency"     => env("CURRENCY", "INR"),
+    "plan2"         => env("PLAN_2", 1499),
+    "planDiscount2" => env("PLAN_DISCOUNT_2", 999),
+    "plan3"         => env("PLAN_3", 2499),
+    "planDiscount3" => env("PLAN_DISCOUNT_3", 1999),
+    "plan4"         => env("PLAN_4", 5999),
+    "planDiscount4" => env("PLAN_DISCOUNT_4", 1000),
+];
